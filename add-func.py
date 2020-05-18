c3ffcd6114c0ec606e9b6df81c3800d946f81418
Sam T <48508802+samm4x@users.noreply.github.com>
2020-05-18 14:11:26 +0100

    Move math funcs into a single file

--- add-func.py
+++
@@ -1,3 +0,0 @@
-def add(num1, num2):
-  answer = num1 + num2
-  return answer
---
+++ math-funcs.py
@@ -0,0 +1,15 @@
+def addition(num1, num2):
+  answer = num1 + num2
+  return answer
+
+def subtract(num1, num2):
+  answer = num1 - num2
+  return answer
+
+def multiply(num1, num2):
+  answer = num1 * num2
+  return answer
+
+def divide(num1, num2):
+  answer = num1 / num2
+  return answer
