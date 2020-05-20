b298565b6653dfe75af71503f00c1620beac1938
Sam T <48508802+samm4x@users.noreply.github.com>
2020-05-20 14:06:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from math-funcs import addition, subtract, divide, multiply
-nums = []
 preAnswer = 0
-
-print("Welcome to the python calculator program.")
+oMenu = True
+while oMenu:
+  print("Welcome to the python calculator program.")
+  print("")
