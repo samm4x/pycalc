8b7557b189a55b914e8a3a68b0be00292f805a20
Sam T <48508802+samm4x@users.noreply.github.com>
2020-05-20 13:29:52 +0100

    Update math-funcs.py

--- math-funcs.py
+++ math-funcs.py
@@ -1,6 +1,7 @@
-def addition(num1, num2):
-  answer = num1 + num2
-  return answer
+def addition(num1, *nums): 
+    for num in nums: 
+        answer = num1 + *nums
+    return answer
 
 def subtract(num1, num2):
   answer = num1 - num2
@@ -13,6 +14,3 @@ def multiply(num1, num2):
 def divide(num1, num2):
   answer = num1 / num2
   return answer
-
-def pi():
-  from cmath import pi
