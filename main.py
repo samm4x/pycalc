274da832a821b5f1fa49fd39a73f6b822d21348d
Sam T <48508802+samm4x@users.noreply.github.com>
2020-05-20 14:48:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,23 @@ from previous-answer import *
 oMenu = True
 while oMenu:
   print("Welcome to the python calculator program.")
-  print("")
+  print("""Currently availabe operations:
+  a = Addition
+  s = Subtraction
+  m = Multiplication 
+  d = Divison""")
+  operation = input("What operation do you want to perform? (a/s/m/d) ").lower()
+  numbers = []
+  moreNumbers = True
+  if operation == "a":
+    while moreNumbers:
+      numbers.append(float(input("Number: ")))
+      yn = input("Do you want to add another number to the calculation? (y/n)").lower()
+      if yn == "y":
+        moreNumbers = True
+        return
+      if yn == "n":
+        moreNumbers = False
+    answer = addition(*numbers)
+      
+print(answer)
